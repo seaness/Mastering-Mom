ff67d0bac59dbb8ad761c76fa2847b73facd6275
Sean Samenfeld-Specht <sean.samenfeldspecht@west.cmu.edu>
2011-09-06 08:25:44 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MasteringMom::Application.config.secret_token = '4d6d5bfb006a755ff67140dea3b18cb665b5fb45eda7a8de450020c80d68e95731f1abbd8afc461b7cff97f808f89563518a9754b4bdab043b09a795faf21445'
