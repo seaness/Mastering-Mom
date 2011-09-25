499e69696b96e87fdcc986b38db934b83cc083ee
Sean Samenfeld-Specht <sean.samenfeldspecht@west.cmu.edu>
2011-09-25 12:59:50 -0700

    Final touches before turning in.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110923072408) do
+ActiveRecord::Schema.define(:version => 20110925183143) do
 
   create_table "blog_posts", :force => true do |t|
     t.text     "content"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20110923072408) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                                 :default => false
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
